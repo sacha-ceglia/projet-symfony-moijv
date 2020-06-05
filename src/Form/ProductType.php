2e4c28145c3be9675328dc570f75cf99d1ddc168
Thibault Truffert <thibaulttruffert@hotmail.com>
2020-06-05 11:17:26 +0200

    stale before upgrade

--- src/Form/ProductType.php
+++ src/Form/ProductType.php
@@ -4,7 +4,6 @@ namespace App\Form;
 
 use App\Entity\Product;
 use App\Entity\Tag;
-use PUGX\AutocompleterBundle\Form\Type\AutocompleteType;
 use Symfony\Component\Form\Extension\Core\Type\FileType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\AbstractType;
@@ -13,6 +12,7 @@ use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Validator\Constraints\Image;
+use Tetranz\Select2EntityBundle\Form\Type\Select2EntityType;
 
 class ProductType extends AbstractType
 {
@@ -38,8 +38,18 @@ class ProductType extends AbstractType
                     'placeholder' => "Saisir votre description"
                 ]
             ])
-            ->add('tags', AutocompleteType::class, [
-                'class' => Tag::class
+            ->add('tags', Select2EntityType::class, [
+                'class' => Tag::class,
+                'multiple' => true,
+                'remote_route' => 'tag',
+                'text_property' => 'name',
+                'cache' => false,
+                'language' => 'fr',
+                'placeholder' => 'Sélectionner un ou plusieurs tags',
+                'allow_add' => [
+                    'enabled' => true,
+                    'tag_separators' => '[","]'
+                ]
             ])
         ;
     }
